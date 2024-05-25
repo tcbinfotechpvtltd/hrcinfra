06905e87fbec0d202099a25beecc4ccfe3fce8a1
ajayraj5 <ajay.mahiwal@tcbinfotech.co.in>
2024-05-25 16:49:01 +0530

    feat:Created 2 warehouses for each site and added field in Warehouse for type of it

--- hrcinfra/hrc_infra/doctype/site/site.py
+++ hrcinfra/hrc_infra/doctype/site/site.py
@@ -1,9 +1,22 @@
 # Copyright (c) 2024, TCB InfoTech and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Site(Document):
-	pass
+    def after_insert(self):
+        frappe.get_doc({
+            'doctype':'Warehouse',
+            'warehouse_name':self.site_name,
+        }).insert()
+
+        frappe.get_doc({
+            'doctype':'Warehouse',
+            'warehouse_name':self.site_name+" - Scrap",
+            'custom_warehouse__type':"Scrap"
+        }).insert()
+        
+
+    

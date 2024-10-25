b8f2c46e4e5e5d0df481ee405130085279b4c453
ajayraj5 <ajaymahiwal7@gmail.com>
2024-10-25 10:28:33 +0530

    fix: create parent warehouse & store
    while create a project then create a parent(group) warehouse and under that a store warehose

--- hrcinfra/public/py/project_custom.py
+++ hrcinfra/public/py/project_custom.py
@@ -6,12 +6,25 @@ def after_insert(doc, method):
 # get_doc is used for two purposed, to fetch existing documents or create a new document
 # When you pass a dictionary of values with the 'doctype' key, 
 # it creates a new document instance but does not save it until you call .insert() or .save()
-    warehouse_doc= frappe.get_doc({
+    parent_warehouse_doc= frappe.get_doc({
         'doctype':'Warehouse',
         'warehouse_name':doc.project_name,
-        'custom_project_link':doc.name,
+        'custom_project':doc.name, 
+        'is_group': 1,
     })
 
-    warehouse_doc.insert()
+    parent_warehouse_doc.insert()
+
+    
+    store_warehouse_doc= frappe.get_doc({
+        'doctype':'Warehouse',
+        'warehouse_name':f"{doc.project_name}-Store",
+        'custom_project':doc.name, 
+        'parent_warehouse': parent_warehouse_doc.name
+    })
+
+    store_warehouse_doc.insert()
+
+
 
     frappe.msgprint("Warehouse Successfully Created")
